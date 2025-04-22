59ed36af3a38de71fbefd9d7cf5c999ff722b3a0
josue9358 <josueguedes121@gmail.com>
2025-04-21 21:27:55 -0300

    Criado site da Nike

---
+++ Nova pasta/script.js
@@ -0,0 +1,11 @@
+
+let body = document.querySelector("body")
+let tenis = document.querySelector(".img-tenis")
+
+
+
+function mudarVisual(cor, img){
+     body.style.background = cor
+     tenis.src = img
+
+}    
